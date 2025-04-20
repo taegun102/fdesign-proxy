d2d4c0095ff5a0dc0f208bcf3e4e4e72c72a4e88
taegun102 <taegun5678@naver.com>
2025-04-21 02:06:44 +0900

    ? 이미지 생성 기능 연결 및 배포 성공

--- app/generate/page.js
+++ app/generate/page.js
@@ -141,8 +141,7 @@ export default function GeneratePage() {
       setPromptText(koreanPrompt);
       const translated = await translateToEnglish(koreanPrompt);
   
-      const response = await fetch("https://us-central1-fdesign-b.cloudfunctions.net/generateImage", {
-        method: "POST",
+      const response = await fetch('https://us-central1-fdesign-b.cloudfunctions.net/generateImage', {
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ prompt: translated, uid: user.uid }),
       });
