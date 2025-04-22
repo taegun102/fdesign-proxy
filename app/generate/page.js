76400180eade94aa707002ecf3b6c96387c51e2a
taegun102 <taegun5678@naver.com>
2025-04-22 20:29:54 +0900

    메시지

--- app/generate/page.js
+++ app/generate/page.js
@@ -141,7 +141,7 @@ export default function GeneratePage() {
       setPromptText(koreanPrompt);
       const translated = await translateToEnglish(koreanPrompt);
   
-      const response = await fetch("https://generateimage-wp7vug75xq-uc.a.run.app/generate", {
+      const response = await fetch('https://generateimage-669367289017.us-central1.run.app/generate', {
         method: "POST", // ✅ 이 줄 꼭 필요!
         headers: {
           "Content-Type": "application/json",
